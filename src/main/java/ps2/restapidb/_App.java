bad65883267e28048bcdbc6195145c5703bd9d24
CarlosQuintieri <carlosequintieri@gmail.com>
2022-06-14 00:50:41 -0300

    teste

--- src/main/java/ps2/restapidb/_App.java
+++ src/main/java/ps2/restapidb/_App.java
@@ -8,19 +8,6 @@ public class _App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(_App.class, args);
-		Avaliacao teste = new Avaliacao();
-
-		teste.setId(1);
-		teste.setEstrela(1);
-		teste.setId(2);
-		teste.setEstrela(2);
-		teste.setId(3);
-		teste.setEstrela(3);
-		teste.setId(4);
-		teste.setEstrela(4);
-		teste.setId(5);
-		teste.setEstrela(5);
-
 	}
 
 }
